3ceefcbea4df6b50c4cd690cb204002d18d8538c
Tim Duckett <tim@timduckett.co.uk>
2014-07-16 14:42:52 +0200

    Initial Commit

---
+++ BaseCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseCollectionView
+//
+//  Created by Tim on 16/07/14.
+//  Copyright (c) 2014 Charismatic Megafauna Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaseCollectionView/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  BaseCollectionView
+//
+//  Created by Tim on 16/07/14.
+//  Copyright (c) 2014 Charismatic Megafauna Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+            
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
